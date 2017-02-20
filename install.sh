57168e71d0372d83485acad036bfe6ecdf8484c3
bluer.hu <bluer.hu@trantect.com>
2017-02-21 01:17:44 +0800

    完善install.sh安装脚本, 支持安装.ideavimrc文件

--- install.sh
+++ install.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
-
 sudo cp ./vimrc /etc/vim/vimrc
-sudo cp ./monokai.vim /usr/share/vim/vim74/colors/
-sudo cp ./Tomorrow-Night-Eighties.vim /usr/share/vim/vim74/colors/
+ls /usr/share/vim/vim74/colors/ 1>/dev/null 2>/dev/null && sudo cp ./monokai.vim /usr/share/vim/vim74/colors/
+ls /usr/share/vim/vim80/colors/ 1>/dev/null 2>/dev/null && sudo cp ./monokai.vim /usr/share/vim/vim80/colors/
+ls /usr/share/vim/vim74/colors/ 1>/dev/null 2>/dev/null && sudo cp ./Tomorrow-Night-Eighties.vim /usr/share/vim/vim74/colors/
+ls /usr/share/vim/vim80/colors/ 1>/dev/null 2>/dev/null && sudo cp ./Tomorrow-Night-Eighties.vim /usr/share/vim/vim80/colors/
+sudo cp ./vimrc ~/.ideavimrc
+
+ls /usr/share/vim/vim74/colors/ 1>/dev/null 2>/dev/null && echo 'You have installed vim7.4!'
+ls /usr/share/vim/vim80/colors/ 1>/dev/null 2>/dev/null && echo 'You have installed vim8.0!'
+echo 'Vimrc and .ideavimrc install finished!'
