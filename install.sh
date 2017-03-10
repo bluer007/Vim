1a911c23fa7e50b9c6dbf93d9d091d54af823313
bluer.hu <bluer.hu@trantect.com>
2017-03-10 09:54:01 +0800

    安装脚本新增备份vimrc功能

--- install.sh
+++ install.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 
+sudo cp /etc/vim/vimrc /etc/vim/vimrc.bak
 sudo cp ./vimrc /etc/vim/vimrc
 ls /usr/share/vim/vim74/colors/ 1>/dev/null 2>/dev/null && sudo cp ./monokai.vim /usr/share/vim/vim74/colors/
 ls /usr/share/vim/vim80/colors/ 1>/dev/null 2>/dev/null && sudo cp ./monokai.vim /usr/share/vim/vim80/colors/
 ls /usr/share/vim/vim74/colors/ 1>/dev/null 2>/dev/null && sudo cp ./Tomorrow-Night-Eighties.vim /usr/share/vim/vim74/colors/
 ls /usr/share/vim/vim80/colors/ 1>/dev/null 2>/dev/null && sudo cp ./Tomorrow-Night-Eighties.vim /usr/share/vim/vim80/colors/
+sudo cp ~/.ideavimrc ~/.ideavimrc.bak
 sudo cp ./vimrc ~/.ideavimrc
 
 ls /usr/share/vim/vim74/colors/ 1>/dev/null 2>/dev/null && echo 'You have installed vim7.4!'
 ls /usr/share/vim/vim80/colors/ 1>/dev/null 2>/dev/null && echo 'You have installed vim8.0!'
 echo 'Vimrc and .ideavimrc install finished!'
+echo 'Vimrc: /etc/vim/vimrc     backup: /etc/vim/vimrc.bak'
+echo '.ideavimrc: ~/.ideavimrc     backup:  ~/.ideavimrc.bak'
