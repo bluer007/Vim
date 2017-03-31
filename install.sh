8bfc0a8dbf63c311f4630a668ae18bfeb5701537
bluer.hu <bluer.hu@trantect.com>
2017-03-31 14:57:51 +0800

    新增ideavimrc配置文件 修改install.sh安装脚本

--- install.sh
+++ install.sh
@@ -1,13 +1,17 @@
 #!/bin/bash
 
-sudo cp /etc/vim/vimrc /etc/vim/vimrc.bak
-sudo cp ./vimrc /etc/vim/vimrc
 ls /usr/share/vim/vim74/colors/ 1>/dev/null 2>/dev/null && sudo cp ./monokai.vim /usr/share/vim/vim74/colors/
 ls /usr/share/vim/vim80/colors/ 1>/dev/null 2>/dev/null && sudo cp ./monokai.vim /usr/share/vim/vim80/colors/
 ls /usr/share/vim/vim74/colors/ 1>/dev/null 2>/dev/null && sudo cp ./Tomorrow-Night-Eighties.vim /usr/share/vim/vim74/colors/
 ls /usr/share/vim/vim80/colors/ 1>/dev/null 2>/dev/null && sudo cp ./Tomorrow-Night-Eighties.vim /usr/share/vim/vim80/colors/
+
+#备份配置文件
+sudo cp /etc/vim/vimrc /etc/vim/vimrc.bak
 sudo cp ~/.ideavimrc ~/.ideavimrc.bak
-sudo cp ./vimrc ~/.ideavimrc
+
+#复制配置文件
+sudo cp ./vimrc /etc/vim/vimrc
+sudo cp ./ideavimrc ~/.ideavimrc
 
 ls /usr/share/vim/vim74/colors/ 1>/dev/null 2>/dev/null && echo 'You have installed vim7.4!'
 ls /usr/share/vim/vim80/colors/ 1>/dev/null 2>/dev/null && echo 'You have installed vim8.0!'
